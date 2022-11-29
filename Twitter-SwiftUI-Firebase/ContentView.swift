cd52ed020680228120d5b462f5a4489c631180f8
Samet Koyuncu <sametkoyuncu@live.com>
2022-11-29 12:19:46 +0300

    Initial commit

---
+++ Twitter-SwiftUI-Firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter-SwiftUI-Firebase
+//
+//  Created by fmss on 29.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter-SwiftUI-Firebase/Twitter_SwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_SwiftUI_FirebaseApp.swift
+//  Twitter-SwiftUI-Firebase
+//
+//  Created by fmss on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_SwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
