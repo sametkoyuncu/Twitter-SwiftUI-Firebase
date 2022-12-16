dddc1d4e7e0c993599e03ae198a695bbc8ec3216
Samet Koyuncu <sametkoyuncu@live.com>
2022-12-16 14:57:43 +0300

    Add login UI

---
+++ Twitter-SwiftUI-Firebase/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,104 @@
+//
+//  LoginView.swift
+//  Twitter-SwiftUI-Firebase
+//
+//  Created by fmss on 16.12.2022.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    var body: some View {
+        // parent container
+        VStack {
+            // header view
+            VStack(alignment: .leading) {
+                HStack { Spacer() }
+                
+                Text("Hello.")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                Text("Welcome Back")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+            }
+            .frame(height: 260)
+            .padding(.leading)
+            .background(Color(.systemBlue))
+            .foregroundColor(.white)
+            .clipShape(RoundedShape(corners: .bottomRight))
+            
+            // text areas
+            VStack(spacing: 40) {
+                CustomInputField(imageName: "envelope",
+                                 placeholderText: "Email",
+                                 text: $email)
+                
+                CustomInputField(imageName: "lock",
+                                 placeholderText: "Password",
+                                 text: $password)
+            }
+            .padding(.horizontal, 32)
+            .padding(.top, 44)
+            
+            // forgot your password
+            HStack {
+                Spacer()
+                NavigationLink {
+                    //
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.caption)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                        .padding(.top)
+                        .padding(.trailing, 24)
+                }
+            }
+            
+            //
+            Button {
+                //
+            } label: {
+                Text("Sign In")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5), radius: 10, x: 0, y: 0)
+            
+            Spacer()
+            
+            NavigationLink {
+                RegistrationView()
+                    .navigationBarHidden(true)
+            } label: {
+                HStack {
+                    Text("Don't have an account?")
+                        .font(.footnote)
+                    
+                    Text("Sign Up")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                }
+                .foregroundColor(Color(.systemBlue))
+            }
+            .padding(.bottom, 32)
+        }
+        .ignoresSafeArea()
+        .navigationBarHidden(true)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ Twitter-SwiftUI-Firebase/Core/Components/RoundedShape.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedShape.swift
+//  Twitter-SwiftUI-Firebase
+//
+//  Created by fmss on 16.12.2022.
+//
+
+import SwiftUI
+
+struct RoundedShape: Shape {
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: 80, height: 80))
+        
+        return Path(path.cgPath)
+    }
+}
